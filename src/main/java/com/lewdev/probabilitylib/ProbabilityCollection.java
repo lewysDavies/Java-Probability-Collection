d4b61b0097504cf2f24ee44c35eba7fd0eb80598
lewysryan <lewys.davies@btinternet.com>
2020-07-07 15:22:24 +0100

    innit commit

---
+++ src/main/java/com/lewdev/probabilitylib/ProbabilityCollection.java
@@ -0,0 +1,194 @@
+package com.lewdev.probabilitylib;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * ProbabilityCollection for retrieving random elements based on probability.
+ * <br>
+ * <br>
+ * <b>Selection Algorithm Implementation</b>:
+ * <p>
+ * <ul>
+ * <li>Elements have a "block" of space, sized based on their probability share
+ * <li>"Blocks" start from index 1 and end at the total probability of all elements
+ * <li>A random number is selected between 1 and the total probability
+ * <li>Which "block" the random number falls in is the element that is selected
+ * <li>Therefore "block"s with larger probability have a greater chance of being
+ * selected than those with smaller probability.
+ * </p>
+ * </ul>
+ * 
+ * @author Lewys Davies
+ *
+ * @param <E> Type of elements
+ */
+public class ProbabilityCollection<E> {
+
+	protected final Comparator<ProbabilitySetElement<E>> comparator = 
+			(o1, o2)-> Integer.compare(o1.getIndex(), o2.getIndex());
+	
+	private final TreeSet<ProbabilitySetElement<E>> collection;
+
+	private int totalProbability;
+	
+	/**
+	 * Construct a new Probability Collection
+	 */
+	public ProbabilityCollection() {
+		this.collection = new TreeSet<>(this.comparator);
+		this.totalProbability = 0;
+	}
+
+	/**
+	 * @return Number of objects inside the collection
+	 */
+	public int size() {
+		return this.collection.size();
+	}
+	
+	/**
+	 * @return Collection contains no elements
+	 */
+	public boolean isEmpty() {
+		return this.collection.isEmpty();
+	}
+	
+	/**
+	 * @param object
+	 * @return True if the collection contains the object, else False
+	 */
+	public boolean contains(E object) {
+		return this.collection.stream()
+			.filter(entry -> entry.getObject().equals(object))
+			.findFirst()
+			.isPresent();
+	}
+
+	/**
+	 * @return Iterator over collection
+	 */
+	public Iterator<ProbabilitySetElement<E>> iterator() {
+		return this.collection.iterator();
+	}
+	
+	/**
+	 * Add an object to this collection
+	 * 
+	 * @param object
+	 * @param probability share
+	 */
+	public void add(E object, int probability) {
+		this.collection.add(new ProbabilitySetElement<E>(object, probability));
+		this.totalProbability += probability;
+		this.updateIndexes();
+	}
+
+	/**
+	 * Remove a object from this collection
+	 * 
+	 * @param object
+	 * @return True if object was removed, else False.
+	 */
+	public boolean remove(E object) {
+		Iterator<ProbabilitySetElement<E>> it = this.iterator();
+		boolean removed = false;
+		
+		while(it.hasNext()) {
+			ProbabilitySetElement<E> element = it.next();
+			if(element.getObject().equals(object)) {
+				removed = true;
+				this.totalProbability -= element.getProbability();
+				it.remove();
+			}
+		}
+		
+		this.updateIndexes();
+		return removed;
+	}
+	
+	/**
+	 * @return Random object based on probability
+	 */
+	public E get() {
+		ProbabilitySetElement<E> toFind = new ProbabilitySetElement<>(null, 0);
+		toFind.setIndex(ThreadLocalRandom.current().nextInt(1, this.totalProbability + 1));
+		
+		return this.collection.floor(toFind).getObject();
+	}
+	
+	/**
+	 * @return Sum of all element's probability
+	 */
+	public final int getTotalProbability() {
+		return this.totalProbability;
+	}
+	
+	/*
+	 * Calculate the size of all element's "block" of space: 
+	 * i.e 1-5, 6-10, 11-14, 15, 16
+	 * 
+	 * We then only need to store the start index of each element
+	 */
+	private void updateIndexes() {
+		int previousIndex = 0;
+		
+		for(ProbabilitySetElement<E> entry : this.collection) {
+			previousIndex = entry.setIndex(previousIndex + 1) + (entry.getProbability() - 1);
+		}
+	}
+	
+	/**
+	 * Used internally to store information about a object's
+	 * state in a collection. Specifically, the probability 
+	 * and index within the collection.
+	 * 
+	 * Indexes refer to the start position of this element's "block" of space.
+	 * The space between element "block"s represents their probability of being selected
+	 * 
+	 * @author Lewys Davies
+	 *
+	 * @param <T> Type of element
+	 */
+	final static class ProbabilitySetElement<T> {
+		private final T object;
+		private final int probability;
+		private int index;
+		
+		/**
+		 * @param object
+		 * @param probability
+		 */
+		protected ProbabilitySetElement(T object, int probability) {
+			this.object = object;
+			this.probability = probability;
+		}
+
+		/**
+		 * @return The actual object
+		 */
+		public final T getObject() {
+			return this.object;
+		}
+
+		/**
+		 * @return Probability share in this collection
+		 */
+		public final int getProbability() {
+			return this.probability;
+		}
+		
+		// Used internally, see this class's documentation
+		protected final int getIndex() {
+			return this.index;
+		}
+		
+		// Used Internally, see this class's documentation
+		protected final int setIndex(int index) {
+			this.index = index;
+			return this.index;
+		}
+	}
+}
---
+++ src/test/java/com/lewdev/probabilitylib/ProbabilityCollectionTest.java
@@ -0,0 +1,167 @@
+package com.lewdev.probabilitylib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+/**
+ * @author Lewys Davies
+ */
+public class ProbabilityCollectionTest {
+
+	@RepeatedTest(value = 1000)
+	public void test_insert() {
+		ProbabilityCollection<String> collection = new ProbabilityCollection<>();
+		assertEquals(0, collection.size());
+		assertTrue(collection.isEmpty());
+		assertEquals(0, collection.getTotalProbability());
+		
+		collection.add("A", 2);
+		assertTrue(collection.contains("A"));
+		assertEquals(1, collection.size());
+		assertFalse(collection.isEmpty());
+		assertEquals(2, collection.getTotalProbability()); // 2
+		
+		collection.add("B", 5);
+		assertTrue(collection.contains("B"));
+		assertEquals(2, collection.size());
+		assertFalse(collection.isEmpty());
+		assertEquals(7, collection.getTotalProbability()); // 5 + 2
+
+		collection.add("C", 10);
+		assertTrue(collection.contains("C"));
+		assertEquals(3, collection.size());
+		assertFalse(collection.isEmpty());
+		assertEquals(17, collection.getTotalProbability()); // 5 + 2 + 10
+		
+		for(int i = 0; i < 100; i++) {
+			collection.add("C", 1);
+			
+			assertTrue(collection.contains("C"));
+			assertEquals(4 + i, collection.size()); // 4 + i
+			assertFalse(collection.isEmpty());
+			assertEquals(18 + i, collection.getTotalProbability()); // 5 + 2 + 10 + i
+		}
+	}
+	
+	@RepeatedTest(value = 1000)
+	public void test_remove() {
+		ProbabilityCollection<String> collection = new ProbabilityCollection<>();
+		assertTrue(collection.size() == 0);
+		assertTrue(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 0);
+		
+		String t1 = "Hello";
+		String t2 = "World";
+		String t3 = "!";
+		
+		collection.add(t1, 10);
+		collection.add(t2, 10);
+		collection.add(t3, 10);
+		
+		assertTrue(collection.size() == 3);
+		assertFalse(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 30);
+		
+		collection.remove(t2);
+		
+		assertTrue(collection.size() == 2);
+		assertFalse(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 20);
+		
+		collection.remove(t1);
+		
+		assertTrue(collection.size() == 1);
+		assertFalse(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 10);
+		
+		collection.remove(t3);
+		
+		assertTrue(collection.size() == 0);
+		assertTrue(collection.getTotalProbability() == 0);
+		assertTrue(collection.isEmpty());
+	}
+	
+	@RepeatedTest(value = 1000)
+	public void test_remove_duplicates() {
+		ProbabilityCollection<String> collection = new ProbabilityCollection<>();
+		assertTrue(collection.size() == 0);
+		assertTrue(collection.isEmpty());
+		
+		String t1 = "Hello";
+		String t2 = "World";
+		String t3 = "!";
+		
+		for(int i = 0; i < 10; i++) {
+			collection.add(t1, 10);
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			collection.add(t2, 10);
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			collection.add(t3, 10);
+		}
+		
+		assertTrue(collection.size() == 30);
+		assertFalse(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 300);
+		
+		collection.remove(t2);
+		
+		assertTrue(collection.size() == 20);
+		assertFalse(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 200);
+		
+		collection.remove(t1);
+		
+		assertTrue(collection.size() == 10);
+		assertFalse(collection.isEmpty());
+		
+		assertTrue(collection.getTotalProbability() == 100);
+		collection.remove(t3);
+		
+		assertTrue(collection.size() == 0);
+		assertTrue(collection.isEmpty());
+		assertTrue(collection.getTotalProbability() == 0);
+	}
+
+	@RepeatedTest(3000)
+	public void test_probability() {
+		ProbabilityCollection<String> collection = new ProbabilityCollection<>();
+		
+		assertEquals(0, collection.size());
+		assertTrue(collection.isEmpty());
+		assertEquals(0, collection.getTotalProbability());
+		
+		collection.add("A", 50);
+		collection.add("B", 25);
+		collection.add("C", 10);
+		
+		int a = 0, b = 0, c = 0;
+		int totalGets = 10_0000;
+		
+		for(int i = 0; i < totalGets; i++) {
+			String random = collection.get();
+			
+			if(random.equals("A")) a++;
+			else if(random.equals("B")) b++;
+			else if(random.equals("C")) c++;
+		}
+		
+		double aProb = (double) collection.getTotalProbability() / 50.0;
+		double bProb = (double) collection.getTotalProbability() / 25.0;
+		double cProb = (double) collection.getTotalProbability() / 10.0;
+		
+		double aResult = (double) totalGets / a;
+		double bResult = (double) totalGets / b;
+		double cResult = (double) totalGets / c;
+		
+		double acceptableDeviation = 0.5; //5 %
+		
+		assertTrue(Math.abs(aProb - aResult) <= acceptableDeviation);
+		assertTrue(Math.abs(bProb - bResult) <= acceptableDeviation);
+		assertTrue(Math.abs(cProb - cResult) <= acceptableDeviation);
+	}
+}
